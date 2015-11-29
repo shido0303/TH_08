a83aebcc2b45923da730e24145b4a8c52a39bdae
abe hiroya <shido0303@docomo.ne.jp>
2015-11-29 13:18:24 +0900

    ビューを追加

---
+++ jphacks/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+        if (Input.GetButton("Jump"))
+        {
+            transform.position += transform.forward * 0.5f;
+        }
+	}
+
+}
